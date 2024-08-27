4aac81711c51763a43f19506f44c4777e6a6c1cd
Hachey <zzf0972@163.com>
2024-08-27 17:14:06 +0800

    [init] Init frontend framework

--- web/frontend/src/router/index.js
+++ web/frontend/src/router/index.js
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LayoutVue from '@/views/Layout.vue'
+import HomeVue from '@/views/home/Home.vue'
+import ListVue from '@/views/list/List.vue'
+import JudgeVue from '@/views/judge/Judge.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      name: 'root',
+      component: LayoutVue,
+      redirect: '/home',
+      children: [
+        { path: '/home', name: 'home', component: HomeVue },
+        { path: '/list', name: 'list', component: ListVue },
+        { path: '/judge', name: 'judge', component: JudgeVue },
+      ]
     }
   ]
 })
