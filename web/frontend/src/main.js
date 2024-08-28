07cec47eebc8a2b983d3e84ea50de1c84f6cf672
Hachey <zzf0972@163.com>
2024-08-28 16:42:23 +0800

    [env] Add global element plus icon support

--- web/frontend/src/main.js
+++ web/frontend/src/main.js
@@ -9,11 +9,15 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import locale from 'element-plus/dist/locale/zh-cn.js'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus, {locale})
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
