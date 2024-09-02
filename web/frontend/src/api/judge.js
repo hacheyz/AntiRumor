d79c29793d7b8327961a8e91b4ac1614c1258f33
Hachey <zzf0972@163.com>
2024-09-02 12:21:45 +0800

    [fix] Fix add question api

--- web/frontend/src/api/judge.js
+++ web/frontend/src/api/judge.js
@@ -4,9 +4,11 @@ export const questionPageListService = (params)=>{
   return request.post('/question/pagelist', params)
 }
 
-export const questionAddService = (params)=>{
-  return request.post('/question/add', params)
-}
+export const questionAddService = (params, query) => {
+  return request.post('/question/add', params, {
+    params: query, // 使用 params 选项传递查询参数
+  });
+};
 
 export const questionAddAgreeService = (id)=>{
   return request.get('/question/addAgree?id='+id)
