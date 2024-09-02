07b8ba501ef544bf77d617a4a739b30be0d8e9e8
Hachey <zzf0972@163.com>
2024-09-02 11:27:00 +0800

    [feat] Add Table component

---
+++ web/frontend/src/components/Table.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-table :data="content" @selection-change="handleSelectionChange"
+            :row-class-name="tableRowClassName"
+            fit="true"
+            ref="childTable"
+            style="width: 100%">
+    <el-table-column v-if="showSelectionColumn"
+                     :selectable="isRowSelectable" type="selection" ></el-table-column>
+    <el-table-column v-for="(column, index) in columns" :key="index"
+                     :label="column.label" :prop="column.prop" v-bind="{ width: column.width || undefined }" :align="column.align" :type="column.type" :sortable="column.sortable">
+      <template v-if="Array.isArray(column.slot)" #default="{ row }">
+        <el-button v-for="(slot, slotIndex) in column.slot" :key="slotIndex"
+                   :icon="slot.icon" circle plain :type="typeof slot.type === 'function' ? slot.type(row) : slot.type"
+                   :class="'full-height-button'"
+                   @click="slot.action(row)"></el-button>
+      </template>
+    </el-table-column>
+    <template #empty>
+      <el-empty description="没有数据"/>
+    </template>
+  </el-table>
+</template>
+
+<script>
+export default {
+  props: {
+    content: Array,
+    title: String,
+    columns: {
+      type: Array,
+      required: true
+    },
+    showSelectionColumn: {
+      type: Boolean,
+      default: false // 设置默认值为false
+    },
+    tableRowClassName: {
+      type: Function
+    },
+    isRowSelectable: {
+      type: Function
+    }
+  },
+  methods: {
+    handleSelectionChange(selectedRows) {
+      this.$emit('selection-change', selectedRows);
+    },
+    handleClearSelection() {
+      if (this.$refs.childTable) {
+        this.$refs.childTable.clearSelection();
+      }
+    },
+  }
+}
+</script>
+
+<style>
+  .el-table .warning-row {
+    --el-table-tr-bg-color: var(--el-color-danger-light-5);
+  }
+  .el-table .success-row {
+    --el-table-tr-bg-color: var(--el-color-success-light-5);
+  }
+</style>
