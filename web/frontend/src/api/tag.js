3da8885378300ecb809926380613a899333ce043
Hachey <zzf0972@163.com>
2024-09-03 08:55:26 +0800

    [feat] Add tag list with relate num api

--- web/frontend/src/api/tag.js
+++ web/frontend/src/api/tag.js
@@ -3,3 +3,7 @@ import request from '@/utils/request.js'
 export const tagListService = (params)=>{
   return request.post('/tag/list', params)
 }
+
+export const tagListWithRelateNumService = (params)=>{
+  return request.post('/tag/listWithRelateNum', params)
+}
