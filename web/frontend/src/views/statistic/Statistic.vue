e7144bdd34e6666cfabd59d8fe1e77886d31de9b
frank <fyxhit2021@163.com>
2024-09-03 09:40:30 +0800

    add some conclusion

--- web/frontend/src/views/statistic/Statistic.vue
+++ web/frontend/src/views/statistic/Statistic.vue
@@ -53,9 +53,21 @@ onMounted(() => {
       <el-col :span="1"/>
     </el-row>
     <el-col>
-      <h1 style="text-align: center">谣言地域关联分析</h1>
     <el-row style="display: flex; justify-content: center">
-      <img class="preview" src="../../assets/province_category_hot.png" width="50%" height="80%">
+      <el-col :span="12">
+        <h1 style="text-align: center">谣言地域-类别关联分析</h1>
+        <img class="preview" src="../../assets/province_category_hot.png" width="90%" height="80%">
+      </el-col>
+      <el-col :span="1"/>
+      <el-col :span="10">
+        <h1 style="text-align: center">统计结论</h1>
+        <p style="width: 100%; font-size: 18px; line-height: 1.6; color: #333; text-align: justify">
+          <strong>1. 谣言类别统计：</strong>突发事件、食药卫生、公共政策和科学常识占9种谣言类别的近九成，其中类别为突发事件的谣言类别最多。<br><br>
+          <strong>2. 谣言地域统计：</strong>在本系统收录的谣言数据集中，重庆、四川、山东、广东、河南、浙江等地的谣言数量较多，该现象可能与地区信息的传播方式等有关<br><br>
+          <strong>3. 谣言地域-类别关联分析：</strong>整体来看，公共安全和突发事件类的谣言在不同省份中较为普遍，显示出这两类谣言是谣言传播中的主要类别。而在一些省份中，谣言类型较为集中，如河南省集中在突发事件类，广东省有关公共政策的谣言较多。<br><br>
+          <strong>4. 总体观察：</strong> 通过这些观察可以初步了解谣言类别的分布情况，各省份谣言的数量对比，以及谣言的地域-分类关联关系，有助于进一步研究这些谣言的产生原因和传播路径。
+        </p>
+      </el-col>
     </el-row>
     </el-col>
   </el-card>
